6c5bf1c35fbcc8b65e4c70deedb5de75b793b1c2
ddaud23 <ddaud@unomaha.edu>
2025-02-10 06:24:35 +0000

    done with RPS

--- RPS.py
+++ RPS.py
@@ -1,7 +1,7 @@
 #RPS.py
-#Name:
+#Name: Daud Daud
 #Date:
-#Assignment:
+#Assignment: lab 3
 import random
 
 def main():
@@ -10,12 +10,49 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
-
+  playAgain = "y"
+  while playAgain == "y":
   #Randomly choose the computer between 'R', 'P', or 'S'
+   computer = random.choice(['R', 'P', 'S']) 
   #Prompt the user for their RPS selection
+   player = input("Pick Rock, Paper, Or Scissors (R,P,S) (Don't forget capilatization!): ")
   #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+  print("computer choose" + computer)
+  print("player choose" + player)
+  if player == "R" and computer == "S":
+    print("player wins!")
+    wins = wins + 1
+  if player == "R" and computer == "P":
+    print("computer wins!")
+    losses = losses + 1
+  if player == "R" and computer == "R": 
+    print("Tie!")
+    ties= ties + 1
+  if player == "P" and computer == "R":
+    print("palyer wins!")
+    wins = wins + 1
+  if player == "P" and computer == "S": 
+    print("computer wins!")
+    wins= wins + 1
+  if player == "P" and computer == "P":
+    print("Tie!")
+    ties = ties + 1
+  if player == "S" and computer == "P":
+    print("Player wins!") 
+    wins = wins + 1
+  if player == "S" and computer == "R":
+    print("Computer wins!")
+    losses = losses + 1
+    if player == "S" and computer == "S":
+      print("Tie!")
+      ties = ties + 1
 
+   
+  
+
+  #Ask the user if they would like to play again.
+  playAgain = input("Would you like to play again? (y/n): ")
+  
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
   print("---- \t ---- \t ------")
